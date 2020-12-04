27d42b71c6af77f457c433b351d75f5b17e8ca63
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2020-12-04 09:23:01 +0800

    init

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Place;
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $places=Place::select('id','title','description','imageUrl')->get();
+        return response()->json($places, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Place $place)
+    {
+        return response()->json($place, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Place $place)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Place $place)
+    {
+        $place->title = $request->title;
+        $place->description = $request->description;
+        $place->category = $request->category;
+        $place->save();
+
+        return response()->json($place, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(Request $request)
+    {
+        $place = Place::find($request->id);
+        $place->delete();
+        $status = 'ok';
+        return response()->json($status, 200);
+    }
+}
